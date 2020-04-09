85deb504259942df4ede514037dd853f69f4fabf
bonn2 <bonifas.fetzer@gmail.com>
2020-04-09 16:55:47 -0400

    Cleaned Up TabComplete.java

--- src/main/java/bonn2/BlockReference/Listeners/TabComplete.java
+++ src/main/java/bonn2/BlockReference/Listeners/TabComplete.java
@@ -11,22 +11,21 @@ public class TabComplete implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> output = new ArrayList<>();
-        if (args.length > 1) { return output; }
-        if (args.length == 0) {
-            output.add("toggle");
-            output.add("look");
-            output.add("clear");
-            if (sender.hasPermission("blockreference.reload")) { output.add("reload"); }
-            return output;
+        switch (args.length) {
+            case 0: {
+                output.add("toggle");
+                output.add("look");
+                output.add("clear");
+                if (sender.hasPermission("blockreference.reload")) { output.add("reload"); }
+            }
+            case 1: {
+                if ("toggle".startsWith(args[0].toLowerCase())) { output.add("toggle"); }
+                if ("look".startsWith(args[0].toLowerCase())) { output.add("look"); }
+                if ("clear".startsWith(args[0].toLowerCase())) { output.add("clear"); }
+                if ("reload".startsWith(args[0].toLowerCase()) && sender.hasPermission("blockreference.reload"))
+                { output.add("reload"); }
+            }
         }
-        if (args.length == 1) {
-            if ("toggle".startsWith(args[0].toLowerCase())) { output.add("toggle"); }
-            if ("look".startsWith(args[0].toLowerCase())) { output.add("look"); }
-            if ("clear".startsWith(args[0].toLowerCase())) { output.add("clear"); }
-            if ("reload".startsWith(args[0].toLowerCase())
-                    && sender.hasPermission("blockreference.reload")) { output.add("reload"); }
-            return output;
-        }
-        return null;
+        return output;
     }
 }
